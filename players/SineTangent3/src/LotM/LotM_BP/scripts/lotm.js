7cfa15f62cd76aae19a932c011d14137d5073b97
SineTangent <sin24steel@gmail.com>
2026-05-31 11:33:26 +0900

    Add LotM, Update InvisibleMini

---
+++ players/SineTangent3/src/LotM/LotM_BP/scripts/lotm.js
@@ -0,0 +1,52 @@
+import { world, system } from "@minecraft/server";
+import { CustomForm, Observable } from "@minecraft/server-ui";
+
+const formTabLists = [
+    `form.lotm.main.status`,
+    `form.lotm.main.shop`,
+    `form.lotm.main.description`
+];
+
+world.beforeEvents.itemUse.subscribe(ev => {
+    const { source } = ev;
+    system.run(() => {
+        const equip = source.getComponent(`minecraft:equippable`);
+        const item = equip.getEquipment(`Mainhand`);
+        if (!item) return;
+        const itemId = item.typeId;
+        switch (itemId) {
+            case `minecraft:emerald`:
+                const title = Observable.create({ translate: `form.lotm.main.title` }, { clientWritable: true });
+                const form = CustomForm.create(source, title);
+                const visibleIndex = Observable.create(-1, { clientWritable: true });
+                for (let i = 0; i < formTabLists.length; i++) {
+                    const visible = Observable.create(visibleIndex.getData() === -1 || visibleIndex.getData() === i, { clientWritable: true })
+                    form.button({ translate: formTabLists[i] }, () => {
+                        visibleIndex.getData() === i ? visibleIndex.setData(-1) : visibleIndex.setData(i);
+                    }, { visible: visible });
+                    visibleIndex.subscribe(newValue => visible.setData(newValue === -1 || newValue === i));
+                };
+                visibleIndex.subscribe(newValue => {
+                    if (newValue === -1) {
+                        title.setData({ translate: `form.lotm.main.title` });
+                    } else {
+                        title.setData({ translate: formTabLists[newValue] });
+                    };
+                });
+                form.show();
+                break;
+        };
+    });
+});
+
+world.afterEvents.playerBreakBlock.subscribe(ev => {
+    const { player, itemStackAfterBreak, dimension } = ev;
+    if (!itemStackAfterBreak) return;
+    const itemId = itemStackAfterBreak.typeId;
+    switch (itemId) {
+        case `minecraft:emerald`:
+            const dp = player.getDynamicProperty(`lotm:data`) === undefined ? `` : JSON.parse(player.getDynamicProperty(`lotm:data`));
+            
+            break;
+    };
+});
