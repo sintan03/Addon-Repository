ef64893b1e986722a4355b5a4b5acccca4460ae4
SineTangent <sin24steel@gmail.com>
2026-05-31 18:07:58 +0900

    Update LotM

--- players/SineTangent3/src/LotM/LotM_BP/scripts/lotm.js
+++ players/SineTangent3/src/LotM/LotM_BP/scripts/lotm.js
@@ -7,6 +7,156 @@ const formTabLists = [
     `form.lotm.main.description`
 ];
 
+const defaultDp = {
+    resource: {
+        block: 0,
+        crystal: 0,
+        platinum: 0,
+        indium: 0,
+        cookie: 0
+    },
+    item: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+};
+
+const itemData = [
+    {
+        // 採掘速度
+        type: `block`,
+        base: 64,
+        multi: 16
+    },
+    {
+        // 獲得量x2
+        type: `block`,
+        base: 256,
+        multi: 8
+    },
+    {
+        // block+10
+        type: `block`,
+        base: 1024,
+        multi: 4
+    },
+    {
+        // 確率+10%
+        type: `crystal`,
+        base: 1,
+        multi: 3
+    },
+    {
+        // block自動+10%
+        type: `crystal`,
+        base: 64,
+        multi: 8
+    },
+    {
+        // 獲得量x2
+        type: `crystal`,
+        base: 256,
+        multi: 16
+    },
+    {
+        // crystal+10
+        type: `crystal`,
+        base: 1024,
+        multi: 4
+    },
+    {
+        // 確率+1000%
+        type: `platinum`,
+        base: 1,
+        multi: 4
+    },
+    {
+        // crystal自動+10%
+        type: `platinum`,
+        base: 64,
+        multi: 8
+    },
+    {
+        // 獲得量x2
+        type: `platinum`,
+        base: 256,
+        multi: 32
+    },
+    {
+        // platinum+10
+        type: `platinum`,
+        base: 1024,
+        multi: 4
+    },
+    {
+        // 確率+100000%
+        type: `indium`,
+        base: 1,
+        multi: 6
+    },
+    {
+        // platinum自動+10%
+        type: `indium`,
+        base: 64,
+        multi: 8
+    },
+    {
+        // 獲得量x2
+        type: `indium`,
+        base: 256,
+        multi: 64
+    },
+    {
+        // indium+10
+        type: `indium`,
+        base: 1024,
+        multi: 4
+    },
+    {
+        // 確率+10000000%
+        type: `cookie`,
+        base: 1,
+        multi: 8
+    },
+    {
+        // indium自動+10%
+        type: `cookie`,
+        base: 64,
+        multi: 8
+    },
+    {
+        // 獲得量x2
+        type: `cookie`,
+        base: 256,
+        multi: 64
+    },
+    {
+        // cookie+10
+        type: `cookie`,
+        base: 1024,
+        multi: 4
+    },
+    {
+        // 全自動+10%
+        type: `cookie`,
+        base: 8192,
+        multi: 16
+    },
+    {
+        // 自動購入
+        type: `cookie`,
+        base: 65536,
+        multi: 65536
+    },
+];
+
+const playerStatus = new Map();
+
+world.afterEvents.playerJoin.subscribe(ev => {
+    const { playerId } = ev;
+    if (!playerStatus.has(playerId)) return;
+    playerStatus.set(playerId, {
+        
+    });
+});
+
 world.beforeEvents.itemUse.subscribe(ev => {
     const { source } = ev;
     system.run(() => {
@@ -46,7 +196,7 @@ world.afterEvents.playerBreakBlock.subscribe(ev => {
     switch (itemId) {
         case `minecraft:emerald`:
             const dp = player.getDynamicProperty(`lotm:data`) === undefined ? `` : JSON.parse(player.getDynamicProperty(`lotm:data`));
-            
+
             break;
     };
 });
